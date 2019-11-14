b5b1e7b5ec8dba4503ff8c55cda8c8f7f24a9c23
Morten Ottersen <mo@eiendomsverdi.no>
2019-11-14 16:50:43 +0100

    some testing, need to clean up soon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { mockData } from './test.model';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
   testheaders = ["h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11"];
 
+  hidden:boolean = true;
+   testData = mockData;
+  model: {
+    headers: string[];
+    rows: any[][];
+  } = {
+      headers: [],
+      rows: []
+    }
 
+
+    showPopup($event:MouseEvent) {
+      $event.stopPropagation();
+      this.hidden = !this.hidden;
+     }
+       
+
+    ngOnInit(): void {
+      const numOfColumns = 10;
+      const numberOfRows = 10;
+  
+  
+      for (let i = 0; i < numOfColumns; i++) {
+        const headers = `header${i}`;
+        this.model.headers.push(headers);
+  
+        for (let j = 0; j < numberOfRows; j++) {
+          const row = `row${j}`;
+          const cells = [];
+          for (let f = 0; f < numOfColumns; f++) {
+            const cell = `cell ${f} in  ${row} ${Math.floor((Math.random() * 1000000000) + 1)}`;
+            cells.push(cell);
+          }
+          this.model.rows.push(cells);
+  
+        }
+      }
+    }
 
   testdata = {
     testheaders:["h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11"],
+
+
+
+    
+
     rows: [
       {
         testd: [
--- src/app/responsive-table-div/responsive-table-div.component.ts
+++ src/app/responsive-table-div/responsive-table-div.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-responsive-table-div',
   templateUrl: './responsive-table-div.component.html',
-  styleUrls: ['./responsive-table-div.component.scss']
+  styleUrls: ['./responsive-table-div.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ResponsiveTableDivComponent implements OnInit {
 
+  @Input()
+  headers:string[] = [];
+
+
   constructor() { }
+
+  onRowClick() {
+    console.log("click");
+  }
 
   ngOnInit() {
     const numOfColumns = 10;
