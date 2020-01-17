d052df0a9f938010607338b98cdd7321b81f2b14
Morten Ottersen <mo@eiendomsverdi.no>
2020-01-17 14:42:06 +0100

    added updated mixins

--- src/app/tester/tester.component.ts
+++ src/app/tester/tester.component.ts
@@ -10,10 +10,24 @@ export class TesterComponent implements OnInit {
 
 
   testData = {...mockData};
+  mapped:any[];
+
+  dir:boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+    this.mapped = this.testData.data.map(d => ({row:d, show:false}));
+  }
+
+  onSort(selectedField) {
+    this.dir = !this.dir;
+    this.mapped.sort((a, b) => {
+      const d = this.dir ? -1 : 1;
+      
+      return a.row[selectedField] > b.row[selectedField] ? -1 * d : 1 * d;
+   
+    });
   }
 
 }
